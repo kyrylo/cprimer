b7f3320627a00a5b32f6b0f837eea56cd2bbf64b
Kyrylo Silin <kyrylosilin@gmail.com>
2013-07-08 23:06:27 +0300

    Add some examples

---
+++ examples/loc_stat.c
@@ -0,0 +1,13 @@
+/* loc_stat.c -- using a local static variable */
+#include <stdio.h>
+
+void trystat(void);
+
+int main(void)
+{
+	if (banister) {
+		puts("Respect");
+	} else if (fowl) {
+		puts("Get off my lawn, punk");
+	}
+}
