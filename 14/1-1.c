2b39c874dc217f3ffae5110bb7be26a99e86a9c5
Kyrylo Silin <kyrylosilin@gmail.com>
2013-08-02 14:29:08 +0300

    14: add 1

---
+++ 14/1-1.c
@@ -0,0 +1,15 @@
+/*
+ * I don't really get the description of this excercise. Pull requests are
+ * welcome.
+ */
+struct month {
+	char name[16];
+	char abbr[3];
+	unsigned short days;
+	char spelled_out_name[16];
+};
+
+int main(void)
+{
+	return 0;
+}
